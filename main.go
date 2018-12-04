a40203214a8f05b3029a0c969817d00d78dea1c8
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-12-05 03:27:05 +0600

    Driver Functions

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/alamin-mahamud/go-solid/dependency-inversion-principle"
+	"github.com/alamin-mahamud/go-solid/open-closed-principle"
+	"github.com/alamin-mahamud/go-solid/single-responsibility-principle"
+)
+
+func main() {
+	single.Run()
+	dependency.Run()
+	open.Run()
+}
